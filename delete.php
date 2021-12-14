eb598931967b6e88adbb795838bba4f2c48d6079
RomanVaskov <romavaskov@gmail.com>
2021-12-14 12:09:35 +0300

    Lesson09 update

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=php_course;", "root", "");
-$sql = "DELETE FROM students WHERE id=:id";
-$statement = $pdo->prepare($sql);
-$statement->execute($_GET);
-
-header('Location: /index.php');
-?>
